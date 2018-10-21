60f487e50906c0655920ffa390af14d083cc62d7
github username <github email address>
2018-10-21 15:53:00 -0400

    fifth commit

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function togglePlay() {
     video.pause();
   }
 
-
 }
 
 function updateButton() {
@@ -30,17 +29,27 @@ function skip() {
 }
 
 function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.name);
   console.log(this.value);
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`
+}
 
 
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change',
+handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove',
 handleRangeUpdate));
